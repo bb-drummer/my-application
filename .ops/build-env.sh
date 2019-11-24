288127f462e72ed0c847d4dfd08202e584c0ac0e
Björn Bartels <bartels@fr-k.de>
2019-11-25 00:37:00 +0100

    Upgrade 2019 pt.6

--- .ops/build-env.sh
+++ .ops/build-env.sh
@@ -9,3 +9,24 @@
 #ln -s /build-env/bower_components ${CI_PROJECT_DIR}/bower_components
 #ln -s /build-env/yarn.lock ${CI_PROJECT_DIR}/yarn.lock
 #ln -s /build-env/package-lock.json ${CI_PROJECT_DIR}/package-lock.json
+
+export ENVIRONMENT=dev;
+export HOSTNAME=${HOST_DEV};
+export PROJECTHOSTNAME=${CI_COMMIT_REF_SLUG}-${HOSTNAME}
+export PROJECTSLUG=${PROJECTSLUG}-${CI_COMMIT_REF_SLUG}
+
+if [ "$CI_COMMIT_REF_SLUG" = "master" ]; then
+  export ENVIRONMENT=staging;
+  export HOSTNAME=${HOST_STAGING};
+  export PROJECTHOSTNAME=${HOSTNAME}
+fi;
+if [ "$CI_COMMIT_REF_SLUG" = "release" ]; then
+  export ENVIRONMENT=production;
+  export HOSTNAME=${HOST_PROD};
+  export PROJECTHOSTNAME=${HOSTNAME}
+fi;
+if [ ! -z "$CI_COMMIT_TAG" ]; then
+  export ENVIRONMENT=production;
+  export HOSTNAME=${HOST_PROD};
+  export PROJECTHOSTNAME=${HOSTNAME}
+fi;
