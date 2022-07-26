100a9c4a31e0a913fc53bb91287501f1c23523d5
Björn Bartels <coding@bjoernbartels.earth>
2022-07-26 02:47:15 +0200

    update2022

---
+++ config/autoload/local.php
@@ -0,0 +1,17 @@
+<?php
+return array(
+    'db' => array( // global database setup
+        'driver'    => 'PdoMysql',
+        #'driver'    => 'PDO_Mysql',
+        #'driver'    => 'pdo_mysql',
+        'hostname'  => $_ENV['MYSQL_HOST'],
+        'database'  => $_ENV['MYSQL_DATABASE'],
+        'username'  => $_ENV['MYSQL_USER'],
+        'password'  => ($_ENV['MYSQL_PASS']),
+    ),
+    'service_manager' => array(
+            'factories' => array(
+                    'Zend\Db\Adapter\Adapter' => 'Zend\Db\Adapter\AdapterServiceFactory',
+            ),
+    ),
+);
